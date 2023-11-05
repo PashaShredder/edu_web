57aa513b8b5e6e6c3e28b399705c11602b145cb7
shredder <lafalarami@gmail.com>
2023-11-05 17:26:32 +0200

    test branch

---
+++ test.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: division by zero"
+    return a / b
+
+# Test the functions
+print("Addition:", add(5, 3))
+print("Subtraction:", subtract(10, 4))
+print("Multiplication:", multiply(7, 2))
+print("Division:", divide(8, 2))
